533f6c36a7e6b94e91ef5a22e80557787229e810
zuberiashaik <168738443+zuberiashaik@users.noreply.github.com>
2024-11-11 17:20:54 +0530

    Add files via upload

---
+++ pattern2.java
@@ -0,0 +1,17 @@
+package Patterns;
+import java.util.Scanner;
+public class pattern2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for (int r=1; r<=n; r++) {
+			for (int c=1; c<=n; c++) {
+				System.out.print(r);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
